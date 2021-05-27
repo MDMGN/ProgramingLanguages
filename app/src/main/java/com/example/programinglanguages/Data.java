2a8197ab815736cfd849c49c7cf52dbac7b79107
MDMGN <michaelmdvrhack@gmail.com>
2021-05-27 17:36:46 +0200

    Splash Activity-MainActivity finality.

--- app/src/main/java/com/example/programinglanguages/Data.java
+++ app/src/main/java/com/example/programinglanguages/Data.java
@@ -19,6 +19,13 @@ public class Data {
         addProgramingLanguages(new ProgramingLanguages(R.string.title_java,R.string.java_info,"java_info_image"));
         addProgramingLanguages(new ProgramingLanguages(R.string.title_javascript,R.string.js_info,"js_info_image"));
         addProgramingLanguages(new ProgramingLanguages(R.string.title_php,R.string.php_info,"php_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_python,R.string.python_info,"python_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_ruby,R.string.ruby_info,"ruby_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_go,R.string.go_info,"go_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_kotlin,R.string.kotlin_info,"kotlin_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_swift,R.string.swift_info,"swift_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_cobol,R.string.cobol_info,"cobol_info_image"));
+        addProgramingLanguages(new ProgramingLanguages(R.string.title_pascal,R.string.pascal_info,"pascal_info_image"));
 
     }
     public static void inicializarDatos(){
